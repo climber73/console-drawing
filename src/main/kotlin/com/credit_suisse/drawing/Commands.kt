e6cc71880a6051aebcb9f4655774d0f033870a55
i.kushmantsev <i.kushmantsev@fun-box.ru>
2019-04-21 12:24:03 +0400

    target help

--- src/main/kotlin/com/credit_suisse/drawing/Commands.kt
+++ src/main/kotlin/com/credit_suisse/drawing/Commands.kt
@@ -4,14 +4,33 @@ interface Command
 
 sealed class AddShapeCommand : Command
 
-data class CreateCanvas(val width: Int, val height: Int) : Command
+data class CreateCanvas(
+    val width: Int,
+    val height: Int
+) : Command
 
-data class AddLine(val x1: Int, val y1: Int, val x2: Int, val y2: Int) : AddShapeCommand()
+data class AddLine(
+    val x1: Int,
+    val y1: Int,
+    val x2: Int,
+    val y2: Int
+) : AddShapeCommand()
 
-data class AddRect(val x1: Int, val y1: Int, val x2: Int, val y2: Int) : AddShapeCommand()
+data class AddRect(
+    val x1: Int,
+    val y1: Int,
+    val x2: Int,
+    val y2: Int
+) : AddShapeCommand()
 
-data class BucketFill(val x: Int, val y: Int, val c: Char) : Command
+data class BucketFill(
+    val x: Int,
+    val y: Int,
+    val c: Char
+) : Command
 
 class Quit : Command
 
-class PrintHelp : Command
+data class PrintHelp(
+    val cmdType: Class<out Command> = Command::class.java
+) : Command
