52d5d60ccdb6e1255bc82a3f8bcfa8d65be50c9f
i.kushmantsev <i.kushmantsev@fun-box.ru>
2019-04-22 11:39:28 +0400

    flat array

--- src/main/kotlin/com/credit_suisse/drawing/console/ConsoleCanvas.kt
+++ src/main/kotlin/com/credit_suisse/drawing/console/ConsoleCanvas.kt
@@ -5,53 +5,32 @@ import com.credit_suisse.drawing.*
 const val MAX_WIDTH = 80
 const val MAX_HEIGHT = 80
 
-
 class ConsoleCanvas(
     override val width: Int,
     override val height: Int
 ) : Canvas<ConsolePoint, Char> {
 
-    private val state: Array<Array<Char>> // array of rows (not columns), so Y is first coordinate
+    private val state: Array<Char> // rows of points go one by one
 
     init {
         require(width in 1..MAX_WIDTH) { "Canvas width should be in [1..$MAX_WIDTH]" }
         require(height in 1..MAX_HEIGHT) { "Canvas height should be in [1..$MAX_HEIGHT]" }
-        state = Array(height) { Array(width) { BLANK_COLOR } }
+        state = Array(height * width) { BLANK_COLOR }
     }
 
-    override fun contains(p: ConsolePoint): Boolean {
-        return p.x in (1..width) && p.y in (1..height)
-    }
+    override fun contains(p: ConsolePoint) =
+        p.x in (1..width) && p.y in (1..height)
 
     // todo : test!
-    override fun contains(shape: ConsoleShape): Boolean {
-        return shape.points().all { contains(it) }
-    }
+    override fun contains(shape: ConsoleShape) =
+        shape.points().all { contains(it) }
 
-    override fun iterator(): Iterator<Char> {
-        return object : Iterator<Char> {
-            private var y = 0
-            private var x = 0
-
-            override fun hasNext(): Boolean {
-                return y < height && x < width
-            }
-
-            override fun next(): Char {
-                val next = state[y][x++]
-                if (x >= width) {
-                    x = 0
-                    y++
-                }
-                return next
-            }
-        }
-    }
+    override fun iterator() = state.iterator()
 
     override fun add(shape: ConsoleShape) {
         require(this.contains(shape)) { "$shape doesn't fit canvas" }
         for (p in shape.points()) {
-            state[p.y-1][p.x-1] = DEFAULT_COLOR
+            state[(p.y-1)*width + (p.x-1)] = DEFAULT_COLOR
         }
     }
 
@@ -59,19 +38,19 @@ class ConsoleCanvas(
         require(contains(p)) { "Point $p doesn't fit canvas" }
         val x = p.x
         val y = p.y
-        val original = state[y - 1][x - 1]
+        val original = state[(y - 1)*width + (x - 1)]
         bucketFill(x - 1, y - 1, original, c)
     }
 
     private fun bucketFill(x: Int, y: Int, original: Char, c: Char) {
-        state[y][x] = c
-        if (y - 1 >= 0 && state[y - 1][x] == original)
+        state[y*width + x] = c
+        if (y - 1 >= 0 && state[(y - 1)*width + x] == original)
             bucketFill(x, y - 1, original, c)
-        if (y + 1 < height && state[y + 1][x] == original)
+        if (y + 1 < height && state[(y + 1)*width + x] == original)
             bucketFill(x, y + 1, original, c)
-        if (x - 1 >= 0 && state[y][x - 1] == original)
+        if (x - 1 >= 0 && state[y*width + (x - 1)] == original)
             bucketFill(x - 1, y, original, c)
-        if (x + 1 < width && state[y][x + 1] == original)
+        if (x + 1 < width && state[y*width + (x + 1)] == original)
             bucketFill(x + 1, y, original, c)
     }
 
